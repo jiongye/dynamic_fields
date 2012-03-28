6c9596e1a241dbab23abd74521438d4797ed1872
Jiongye Li <jiongye@gmail.com>
2012-03-28 15:14:52 -0400

    moved file around a bit

--- app/assets/javascripts/dynamic_fields/jquery.dynamic-fields.js
+++ app/assets/javascripts/dynamic_fields/jquery.dynamic-fields.js
@@ -1,25 +1,3 @@
-jQuery(function(){
-
-  $("body").delegate(".remove_dynamic_field", 'click', function(){
-    var destroy_field = $(this).parent().find("input[name$='[_destroy]']");
-
-    if (destroy_field[0] != undefined) {
-      if (confirm("Are you sure?")) {
-        destroy_field.val('true');
-        $(this).parent().hide();
-      }
-    }else {
-      if ($(this).parent().siblings().find(".remove_dynamic_field")[0] != undefined)
-        $(this).parent().remove();
-      else
-        $(this).parent().hide();
-    }
-
-    return false;
-  });
-
-});
-
 (function($) {
 
   $.fn.cloneFields = function(opts){
